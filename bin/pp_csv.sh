d07b7cc93889edef62714ab96444caa614ca1149
Sebastia Gassner <sebastian.gassner@gmail.com>
2013-10-14 08:55:24 +0200

    adding FIT tools and documentation

---
+++ bin/pp_csv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# != 1 ]; then
+    echo "Usage: $(basename $0) CSV_FILE"
+    exit 2
+fi
+
+FILE="$1"
+if [ ! -f "$FILE" ]; then
+    echo "Cannot read file '$FILE'."
+    exit 1
+fi
+
+sed '1d' "$FILE" | \
+awk -v columns="$(head -1 ${FILE})" -F ',' 'BEGIN{ split(columns,c,",") }{ for(i=1;i<=NF;i++){ printf("%s: %s\n", c[i], $i) }; print "--------"}'
